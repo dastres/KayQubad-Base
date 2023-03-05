9749a5c5bd61b346e98788b6bda48638652be4c8
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-05 14:27:35 +0330

    bugfix (auto generate slug)

--- service/models/model_service.py
+++ service/models/model_service.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 
 from ckeditor_uploader.fields import RichTextUploadingField
 from utils.general.models import DateBasic, Status, Seo, LanguageStatus, BasicPost
+from utils.unique_slug_generator import unique_slug_generator
 
 
 class Service(DateBasic, Seo, Status, LanguageStatus, BasicPost):
@@ -21,3 +24,11 @@ class Service(DateBasic, Seo, Status, LanguageStatus, BasicPost):
 
     def __str__(self):
         return self.title
+
+
+@receiver(pre_save, sender=Service)
+def pre_post_service_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+    if not instance.thumbnail_alt:
+        instance.thumbnail_alt = unique_slug_generator(instance)
