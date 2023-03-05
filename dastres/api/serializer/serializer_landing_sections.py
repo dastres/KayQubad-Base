59b3f1c9d7c1d36c093b0dea3aa20a3807657dbd
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-05 16:28:35 +0330

    add read_only_fields in serializer

--- dastres/api/serializer/serializer_landing_sections.py
+++ dastres/api/serializer/serializer_landing_sections.py
@@ -24,3 +24,4 @@ class LandingSectionsSerializer(serializers.ModelSerializer):
             'title_three',
             'count_three',
         )
+        read_only_fields = ('id', 'created_at', 'updated_at',)
