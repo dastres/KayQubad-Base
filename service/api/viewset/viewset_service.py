57a820bbb5eca53c35382939b62003508ee43203
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-05 14:02:42 +0330

    add DetailSerializer in viewset

--- service/api/viewset/viewset_service.py
+++ service/api/viewset/viewset_service.py
@@ -2,14 +2,22 @@ from rest_framework import viewsets
 
 from service.models import Service
 from service.api.serializer import (
-    ServiceListSerializer
+    ServiceListSerializer, ServiceDetailSerializer
 )
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
     model = Service
-    serializer_class = ServiceListSerializer
+    serializer_class = ServiceDetailSerializer
 
     def get_queryset(self):
         queryset = self.model.objects.all()
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ServiceListSerializer
+        elif self.action in ['create', 'update', 'partial']:
+            pass
+
+        return self.serializer_class
