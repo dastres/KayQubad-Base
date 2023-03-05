0593f4f62ba42a76ce6f24d827f860b362ac0f45
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-05 16:01:49 +0330

    write translation LandingSection

---
+++ dastres/translation.py
@@ -0,0 +1,19 @@
+from modeltranslation.translator import register, TranslationOptions
+from dastres.models import LandingSections
+
+
+@register(LandingSections)
+class LandingSectionsTranslationOptions(TranslationOptions):
+    fields = (
+        'title_main_one',
+        'title_main_two',
+        'title_small',
+        'title_big',
+        'short_description',
+        'title_one',
+        'count_one',
+        'title_two',
+        'count_two',
+        'title_three',
+        'count_three',
+    )
