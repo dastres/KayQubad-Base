4c7e819adf16a3d6587f63fb21ee7c5888ec161c
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-19 10:21:28 +0330

    add meta_title and meta_description in api page

--- page/api/serializer/serializer_page_detail.py
+++ page/api/serializer/serializer_page_detail.py
@@ -8,6 +8,6 @@ class PageDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Page
-        fields = ("id", 'title', 'content', 'slug', 'thumbnail', 'thumbnail_alt', 'get_created_at', 'get_updated_at', 'status',
+        fields = ('meta_title','meta_description',"id", 'title', 'content', 'slug', 'thumbnail', 'thumbnail_alt', 'get_created_at', 'get_updated_at', 'status',
                   'is_active', 'custom_template',)
         read_only_fields = ['id', 'get_created_at', 'get_updated_at']
