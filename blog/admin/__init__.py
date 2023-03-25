f392f9d33efca8061f96dc7c2fc88e6e232b9b56
Ali Esmaeili <fromiran98@gmail.com>
2023-03-25 20:41:03 +0330

    update

--- blog/admin/__init__.py
+++ blog/admin/__init__.py
@@ -1,8 +1,8 @@
 # Admin Single Language
 
-from .admin_post import PostAdmin
-from .admin_category import PostCategoryAdmin
-from .admin_comment import PostCommentAdmin
+from .admin_post_translation import PostAdmin
+from .admin_category_translation import PostCategoryAdmin
+from .admin_comment_translation import PostCommentAdmin
 
 # Admin Multi Language
 # from .admin_post_translation import PostAdmin
