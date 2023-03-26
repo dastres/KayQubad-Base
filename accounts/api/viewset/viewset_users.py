1b43a5e92d64f5032f82dac5021f555344e1446d
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-03-26 14:08:43 +0330

    add create user in viewset

---
+++ accounts/api/viewset/viewset_users.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from rest_framework import permissions
+from accounts.api.serializers import UserListSerializer, UserDetailSerializer, UserCreateSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    model = get_user_model()
+    permissions = [permissions.IsAuthenticated]
+    serializer_class = UserDetailSerializer
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return UserListSerializer
+        elif self.action == 'create':
+            return UserCreateSerializer
+        return self.serializer_class
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.AllowAny()]
+        return super().get_permissions()
+
+
