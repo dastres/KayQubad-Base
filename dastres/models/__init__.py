feed314838ba2f3f4537b68011476ba4c4e63852
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-04-01 13:04:57 +0330

    makemigrations apps

--- dastres/models/__init__.py
+++ dastres/models/__init__.py
@@ -1,2 +1,6 @@
 from dastres.models.models_customers import Customers
 from .models_landing_sections import LandingSections
+
+
+from .models_about import About
+from .models_social_media import SocialMedia
