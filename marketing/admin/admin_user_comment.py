82e7ae7c363927e8f7eb18ce59e3431dbf577d5b
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-04-01 16:33:08 +0330

    bugfix comment user

--- marketing/admin/admin_user_comment.py
+++ marketing/admin/admin_user_comment.py
@@ -1,5 +1,6 @@
 # Core Django
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 # My App
 from marketing.models import UserComment
@@ -13,13 +14,14 @@ class UserCommentAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
 
     fieldsets = (
-        ("Main", {'fields': ('name', 'avatar', 'avatar_alt', 'content', 'company', 'rate')}),
-        ("Date", {'fields': ('created_at', "updated_at")}),
-
+        (_("Main_en"), {'fields': ('name_en', 'content_en', 'company_en', 'is_active_en')}),
+        (_("Main_en"), {'fields': ('name_fa', 'content_fa', 'company_fa', 'is_active_fa')}),
+        (_("Main"), {'fields': ('avatar', 'avatar_alt', 'rate')}),
+        (_("Date"), {'fields': ('created_at', "updated_at")}),
     )
-
     add_fieldsets = (
-        ("Main", {'fields': ('name', 'avatar', 'avatar_alt', 'content', 'company', 'rate')}),
-        ("Date", {'fields': ('created_at', "updated_at")}),
-
+        (_("Main_en"), {'fields': ('name_en', 'content_en', 'company_en', 'is_active_en')}),
+        (_("Main_en"), {'fields': ('name_fa', 'content_fa', 'company_fa', 'is_active_fa')}),
+        (_("Main"), {'fields': ('avatar', 'avatar_alt', 'rate')}),
+        (_("Date"), {'fields': ('created_at', "updated_at")}),
     )
