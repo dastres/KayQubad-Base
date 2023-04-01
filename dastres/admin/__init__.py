52af6db62a40639dc721ea0ee200365aee6ab34b
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-04-01 15:33:26 +0330

    write admin about

--- dastres/admin/__init__.py
+++ dastres/admin/__init__.py
@@ -2,3 +2,5 @@ from .admin_landing_sections import LandingSectionsAdmin
 
 from .admin_social_media_inline import SocialMediaInline
 from .admin_team_member import TeamMembersAdmin
+
+from .admin_about import AboutAdmin
