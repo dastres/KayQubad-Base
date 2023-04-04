7ea4c56ca1b7007ea6945d299bb93299640f75b0
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2023-04-04 11:15:39 +0330

    add meta in poroject

--- project/translation.py
+++ project/translation.py
@@ -4,9 +4,9 @@ from project.models import Project, ProjectCategory
 
 @register(ProjectCategory)
 class ProjectCategoryCategoryTranslationOptions(TranslationOptions):
-    fields = ('title', 'description', 'is_active', 'sub_category',)
+    fields = ('title', 'description', 'is_active', 'sub_category','meta_title','meta_description')
 
 
 @register(Project)
 class ProjectTranslationOptions(TranslationOptions):
-    fields = ('title', 'content', 'is_active', 'category',)
+    fields = ('title', 'content', 'is_active', 'category','meta_title','meta_description')
